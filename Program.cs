e3f4a0b9538d2b246b43a73abf82a4b65e9cd274
oğuz kaan coşkun <koncilya52@gmail.com>
2022-07-20 15:51:07 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace if_else_if{
+    class Program {
+        public static void Main (string[] args){
+            
+            Console.Clear();
+            int time = DateTime.Now.Hour;
+
+
+            if(time >=6 && time < 11){
+                Console.WriteLine("günaydın");
+
+            }
+            else if (time <= 18)
+
+            {
+                Console.WriteLine("iyi günler.");
+            }
+
+            else {
+                Console.WriteLine("iyi geceler");
+            }
+
+            string sonuc = time <=18 ? "iyi günler" : "iyi geceler";
+
+            sonuc = time >= 6 && time<11 ? "günaydın" : time <=18 ? "iyi günler" : "iyi geceler";
+
+            Console.WriteLine(sonuc);
+        }
+    }
+}
